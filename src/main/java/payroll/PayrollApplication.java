33ca70d36ae3d2fcd01cb9768598c413b91d57af
Cantor Alexandru <alexandru.cantor@yopeso.com>
2023-04-02 13:28:11 +0300

    Add Swagger

--- src/main/java/payroll/PayrollApplication.java
+++ src/main/java/payroll/PayrollApplication.java
@@ -1,10 +1,15 @@
 package payroll;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class PayrollApplication{
 	public static void main(String... args) {
 		SpringApplication.run(PayrollApplication.class, args);
