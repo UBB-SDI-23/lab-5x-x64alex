7aea59bb4b3179f3e7c18133e879cfbcec6d283c
Cantor Alexandru <alexcantor64@gmail.com>
2023-05-07 14:58:02 +0300

    Add getUserProfile and getUserStatistics

--- src/main/java/payroll/Repository/UserRepository.java
+++ src/main/java/payroll/Repository/UserRepository.java
@@ -1,6 +1,8 @@
 package payroll.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import payroll.Model.User.User;
 
 import java.util.Optional;
@@ -9,4 +11,25 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Boolean existsByUsername(String username);
+
+    @Query(value = "select count(*) from Category c where c.user.id = :userId")
+    Integer getNumberCategory(
+            @Param("userId") Long userId
+    );
+
+    @Query(value = "select count(*) from Product p where p.user.id = :userId")
+    Integer getNumberProducts(
+            @Param("userId") Long userId
+    );
+
+    @Query(value = "select count(*) from Client c where c.user.id = :userId")
+    Integer getNumberClients(
+            @Param("userId") Long userId
+    );
+
+    @Query(value = "select count(*) from Transaction t where t.user.id = :userId")
+    Integer getNumberTransactions(
+            @Param("userId") Long userId
+    );
+
 }
