db1fd87a8a0245f014dc196a9a9779be7377c536
Cantor Alexandru <alexcantor64@gmail.com>
2023-05-08 06:11:48 +0300

    Add autocomplete

--- react_fr/src/components/AdminDashboard.tsx
+++ react_fr/src/components/AdminDashboard.tsx
@@ -1,8 +1,10 @@
-import { Container, Card, CardContent, IconButton, CardActions, Button, Stack } from "@mui/material";
+import { Container, Card, CardContent, IconButton, CardActions, Button, Stack, Autocomplete, TextField } from "@mui/material";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import axios from "axios";
 import { BACKEND_API_URL, authorization } from "../constants";
+import { UserNameDTO } from "../models/Login/UserNameDTO";
+import { useEffect, useState } from "react";
 
 export const AdminDashboard = () => {
 	const { transactionId } = useParams();
@@ -22,6 +24,21 @@ export const AdminDashboard = () => {
 		  Authorization: authorization,
 		}
 	}
+    const [users, setUsers] = useState<UserNameDTO[]>([]);
+    const [userId, setUserId] = useState<Number>(0);
+	const [searchString, setSearchString] = useState("");
+
+
+    useEffect(() => {
+		fetch(`${BACKEND_API_URL}/user/usernames?searchString=${searchString}`)
+			.then((response) => response.json())
+			.then((data) => {
+				setUsers(data);
+			});
+
+
+	}, [searchString]);
+
 	const deleteAllData = async (event: { preventDefault: () => void }) => {
 		event.preventDefault();
 		await axios.get(`${BACKEND_API_URL}/user/deleteAllEntities`, config);
@@ -31,14 +48,14 @@ export const AdminDashboard = () => {
 
     const runScript = async (event: { preventDefault: () => void }) => {
 		event.preventDefault();
-		await axios.get(`${BACKEND_API_URL}/run-script}`, config1);
+		fetch(`${BACKEND_API_URL}/run-script}`, config1);
 		alert("Script finished")
 		navigate("/");
 	};
 
     const editRoles = async (event: { preventDefault: () => void }) => {
 		event.preventDefault();
-		await axios.delete(`${BACKEND_API_URL}/transactions/${transactionId}`, config);
+		await axios.post(`${BACKEND_API_URL}/user/usernames/${userId}`, config);
 		alert("Role has been edited")
 		navigate("/");
 	};
@@ -82,13 +99,51 @@ export const AdminDashboard = () => {
 			</Card>
             <Card>
 				<CardContent>
-                Edit roles
-                <Stack direction="row" spacing={2}   alignItems="center">
-                        
-
-
+                <Stack direction="row" spacing={2}   alignItems="center"> 
+                Edit roles   
                 </Stack>
 
+
+                <form onSubmit={editRoles}>
+						<Autocomplete
+							disablePortal
+							id="combo-box-demo"
+							options={users.map((user) => user.username)}
+							sx={{ mb: 2 }}
+							onChange={(e, value) => {
+								for (let i = 0; i < users.length; i++) {
+									if(users[i].username === value){
+                                        setUserId(users[i].id)
+									}
+								}
+							}}
+							renderInput={(params) => <TextField {...params} label="User Names" 	
+							onChange={(newValue) => {
+								setSearchString(newValue.target.value)
+							}}/>}
+						/>
+
+                        {/* <Autocomplete
+							disablePortal
+							id="combo-box-demo"
+							options={products.map((product) => product.productName)}
+							sx={{ mb: 2}}
+							onChange={(e, value) => {
+								for (let i = 0; i < products.length; i++) {
+									if(products[i].productName === value){
+										transaction.productId = products[i].productId;
+									}
+								}
+							}}
+							renderInput={(params) => <TextField {...params} label="Product Names" 	
+							onChange={(newValue) => {
+								setSearchString(newValue.target.value)
+							}}/>}
+						/> */}
+					
+
+						<Button id = "submitButton" type="submit">Add Transaction </Button>
+					</form>
 				</CardContent>
 				<CardActions>
 					<Button onClick={editRoles}>edit</Button>
--- react_fr/src/models/Login/UserNameDTO.ts
+++ react_fr/src/models/Login/UserNameDTO.ts
@@ -1,4 +1,4 @@
 export interface UserNameDTO {
-    id?:number;
+    id:number;
     username: string;
 }
