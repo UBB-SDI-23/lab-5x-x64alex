58979b817064fea687bf17391b10deb0a65a135a
Cantor Alexandru <alexcantor64@gmail.com>
2023-05-08 03:27:36 +0300

    Update Reigister.tsx

--- react_fr/src/components/login/Reigister.tsx
+++ react_fr/src/components/login/Reigister.tsx
@@ -23,38 +23,6 @@ export const Register = () => {
             birthdate: new Date()
         }
     );
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 
-            username: signup.username,
-            password:signup.password,
-            name: signup.name,
-            gender: signup.gender,
-            bio: signup.bio,
-            location: signup.location,
-            birthdate: signup.birthdate
-        }),
-    };
-
-	const handleRegister = async (event: { preventDefault: () => void }) => {
-        handleButtonClick()
-        if(!error){
-            try{
-                event.preventDefault();
-        
-        
-                const response = await fetch(`${BACKEND_API_URL}/signin`,requestOptions);
-                const code = await response.json();
-        
-                alert("Registration code is: "+code)
-                navigate("/confirmation");
-                }catch(error){
-                    alert("Error")
-                }
-        }
-	};
-
     const handleButtonClick = () => {
         if (signup.password.length < 8) {
           setError(true);
@@ -64,6 +32,28 @@ export const Register = () => {
         }
       };
 
+	const handleRegister = async (event: { preventDefault: () => void }) => {
+        handleButtonClick()
+        if(!error){
+                event.preventDefault();
+        
+                axios.post(`${BACKEND_API_URL}/signin`, signup)
+                .then((response) => {
+                    console.log(response.data);
+                    const code =  response.data;
+                    alert("Registration code is: "+code)
+                    navigate("/confirmation");
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                    alert("Error")
+                  });                  
+
+        }
+	};
+
+
+
 	return (
 		<Container>
 			<Card>
@@ -72,7 +62,7 @@ export const Register = () => {
 						<IconButton component={Link} sx={{ mr: 3 }} to={`/`} >
 							<ArrowBackIcon />
 						</IconButton>{" "}
-                        <h3>LogIn</h3>
+                        <h3>Register</h3>
 					</Stack>
 
 					<form onSubmit={handleRegister}>
@@ -95,6 +85,33 @@ export const Register = () => {
 							onChange={(newValue) => signup.password = newValue.target.value}
                             error={error}
                             helperText={error ? "Password must be at least 8 characters long" : ""}
+						/>
+                        <TextField
+                            type="string"
+							id="name"
+							label="name"
+							variant="outlined"
+							fullWidth
+							sx={{ mb: 2 }}
+							onChange={(newValue) => signup.name = newValue.target.value}
+						/>
+                        <TextField
+                            type="string"
+							id="bio"
+							label="bio"
+							variant="outlined"
+							fullWidth
+							sx={{ mb: 2 }}
+							onChange={(newValue) => signup.bio = newValue.target.value}
+						/>
+                        <TextField
+                            type="string"
+							id="gender"
+							label="gender"
+							variant="outlined"
+							fullWidth
+							sx={{ mb: 2 }}
+							onChange={(newValue) => signup.gender = newValue.target.value}
 						/>
 
 						<Button id = "submitButton" type="submit"> Register </Button>
