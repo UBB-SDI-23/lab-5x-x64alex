66c9b9a2d2a83d894aba7911e0922087d1dc5807
Cantor Alexandru <alexcantor64@gmail.com>
2023-05-19 19:29:32 +0300

    Modify

--- react_fr/src/components/AppMenu.tsx
+++ react_fr/src/components/AppMenu.tsx
@@ -3,7 +3,7 @@ import { Link, useNavigate, useLocation } from "react-router-dom";
 import { isAdmin, isLoggedIn, logOut } from "../constants";
 
 
-const NavBarButtons = () => {
+export const AppMenu = () => {
 	const location = useLocation();
 	const navigate = useNavigate();
 	const path = location.pathname;
@@ -12,76 +12,47 @@ const NavBarButtons = () => {
 		logOut()
 		navigate("/");
 	}
-
-	return (
-	  <>
-		<Button
-		  variant={path.startsWith("/categories") ? "outlined" : "text"}
-		  to="/categories"
-		  component={Link}
-		  color="inherit"
-		  sx={{ mr: 5 }}>
-		  Categories
-		</Button>
-		<Button
-		  variant={path.startsWith("/products") ? "outlined" : "text"}
-		  to="/products"
-		  component={Link}
-		  color="inherit"
-		  sx={{ mr: 5 }}>
-		  Products
-		</Button>
-		<Button
-		  variant={path.startsWith("/clients") ? "outlined" : "text"}
-		  to="/clients"
-		  component={Link}
-		  color="inherit"
-		  sx={{ mr: 5 }}>
-		  Clients
-		</Button>
-		<Button
-		  variant={path.startsWith("/transactions") ? "outlined" : "text"}
-		  to="/transactions"
-		  component={Link}
-		  color="inherit"
-		  sx={{ mr: 5 }}>
-		  Transactions
-		</Button>
-		{!isLoggedIn() && (
-		  <Button
-			id="login"
-			variant={path.startsWith("/login") ? "outlined" : "text"}
-			to="/login"
-			component={Link}
-			color="inherit"
-			sx={{ mr: 5 }}>
-			Login
-		  </Button>
-		)}
-		{isAdmin() && (
-		  <Button
-			variant={path.startsWith("/admin") ? "outlined" : "text"}
-			to="/admin"
-			component={Link}
-			color="inherit"
-			sx={{ mr: 2 }}>
-			Admin
-		  </Button>
-		)}
-		{isLoggedIn() && (
-		  <Button
-			id="logout"
-			onClick={handleLogOut}
-			color="inherit"
-			sx={{ mr: 10 }}>
-			Logout
-		  </Button>
-		)}
-	  </>
-	);
-  };
-
-export const AppMenu = () => {
+	const pages = [
+		{
+			label: "Categories",
+			variant: path.startsWith("/categories") ? "outlined" : "text",
+			to: "/categories"
+		  },
+		  {
+			label: "Products",
+			variant: path.startsWith("/products") ? "outlined" : "text",
+			to: "/products"
+		  },
+		  {
+			label: "Clients",
+			variant: path.startsWith("/clients") ? "outlined" : "text",
+			to: "/clients"
+		  },
+		  {
+			label: "Transactions",
+			variant: path.startsWith("/transactions") ? "outlined" : "text",
+			to: "/transactions"
+		  },
+		  {
+			label: "Login",
+			variant: path.startsWith("/login") ? "outlined" : "text",
+			to: "/login",
+			id: "login",
+			visible: !isLoggedIn()
+		  },
+		  {
+			label: "Admin",
+			variant: path.startsWith("/admin") ? "outlined" : "text",
+			to: "/admin",
+			visible: isAdmin()
+		  },
+		  {
+			label: "Logout",
+			onClick: handleLogOut,
+			id: "logout",
+			visible: isLoggedIn()
+		  }
+	]
 
 	return (
 		<Box sx={{ flexGrow: 1 }}>
@@ -105,8 +76,60 @@ export const AppMenu = () => {
 					>
 						Payroll
 					</Typography>
+					{/* <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+						<IconButton
+						size="large"
+						aria-label="account of current user"
+						aria-controls="menu-appbar"
+						aria-haspopup="true"
+						onClick={handleOpenNavMenu}
+						color="inherit"
+						>
+						<MenuIcon />
+						</IconButton>
+						<Menu
+						id="menu-appbar"
+						anchorEl={anchorElNav}
+						anchorOrigin={{
+							vertical: "bottom",
+							horizontal: "left"
+						}}
+						keepMounted
+						transformOrigin={{
+							vertical: "top",
+							horizontal: "left"
+						}}
+						open={Boolean(anchorElNav)}
+						onClose={handleCloseNavMenu}
+						sx={{
+							display: { xs: "block", md: "none" }
+						}}
+						>
+						{pages.map((page) => (
+							<MenuItem key={page} onClick={handleCloseNavMenu}>
+								<Typography textAlign="center">{page}</Typography>
+							</MenuItem>
+						))}
+						</Menu>
+					</Box> */}
 					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-						<NavBarButtons />
+					{pages.map((button, index) => (
+						button.visible && (
+						<Button
+							key={index}
+							variant={button.variant as "outlined" | "text" | "contained"}
+							to={button.to as "string"}
+							component={Link}
+							color="inherit"
+							sx={{ mr: 5 }}
+							onClick={button.onClick}
+							id={button.id}
+						>
+							{button.label}
+						</Button>
+						)
+					))}
+
 					</Box>
 				</Toolbar>
 			</AppBar>
