fdd092567171b6014271dfc05fc9437c361135b6
Cantor Alexandru <alexcantor64@gmail.com>
2023-05-20 12:41:58 +0300

    Update AllProducts.tsx

--- react_fr/src/components/products/AllProducts.tsx
+++ react_fr/src/components/products/AllProducts.tsx
@@ -166,10 +166,10 @@ export const AllProducts = () => {
 				</TableContainer>
 			)}
 			{!loading && products.length > 0 && (
-				<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }}}>
-					<Stack direction="column" spacing={1} alignItems="left" >
+				<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none", width:'100%'}}}>
+					<Stack direction="column" spacing={1} alignItems="left" sx={{ width:'100%'}} >
 						{products.map((product: ProductTransactions, index) => (
-							<Stack bgcolor="grey.200" p={2} direction="row" spacing={20} justifyContent="space-between" alignItems="center" sx={{ borderRadius: '6px', width:'100%'}} >
+							<Stack bgcolor="grey.200" p={2} direction="row" spacing={10} justifyContent="space-between" alignItems="center" sx={{ borderRadius: '6px', width:'100%'}} >
 								<Stack direction="column" spacing={2}   alignItems="left">
 									<Typography variant="subtitle1" component="div">
 										#
